b8a0922e2ebbb611105d423158b0eb4088010f80
compsci <compsci@compsci>
2018-03-14 10:46:11 -0600

    fin

--- src/mousedrawing/MouseDrawing.java
+++ src/mousedrawing/MouseDrawing.java
@@ -3,6 +3,8 @@ package mousedrawing;
 import com.sun.glass.events.WindowEvent;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.geom.Line2D;
+import java.awt.geom.Rectangle2D;
 import javax.swing.*;
 
 import javax.swing.*;
@@ -21,8 +23,16 @@ public class MouseDrawing extends JFrame {
      */
     JMenuBar mainMenuBar = new JMenuBar();
     JMenu fileMenu = new JMenu("file");
+    JMenu canvasMenu = new JMenu("Canvas");
     JMenuItem newMenuItem = new JMenuItem("Clear");
-    JMenuItem exitMenuItem = new JMenuItem("exit");
+    JMenuItem exitMenuItem = new JMenuItem("Exit");
+    JMenuItem resizeMenuItem = new JMenuItem("Resize");
+
+    JMenuItem blueMenuItem = new JMenuItem("Blue");
+    JMenuItem redMenuItem = new JMenuItem("Red");
+    JMenuItem magentaMenuItem = new JMenuItem("Magenta");
+    JMenuItem greenMenuItem = new JMenuItem("Green");
+
     JPanel drawPanel = new JPanel();
     JLabel leftColorLabel = new JLabel();
     JLabel rightColorLabel = new JLabel();
@@ -31,6 +41,8 @@ public class MouseDrawing extends JFrame {
     Graphics2D g2D;
     double xPrevious, yPrevious;
     Color drawColor, leftColor, rightColor;
+    int Panelx = 500;
+    int Panely = 400;
 
     /**
      *
@@ -62,14 +74,20 @@ public class MouseDrawing extends JFrame {
         setJMenuBar(mainMenuBar);
         fileMenu.setMnemonic('F');
         mainMenuBar.add(fileMenu);
+        mainMenuBar.add(canvasMenu);
+        canvasMenu.add(blueMenuItem);
+        canvasMenu.add(redMenuItem);
+        canvasMenu.add(magentaMenuItem);
+        canvasMenu.add(greenMenuItem);
         fileMenu.add(newMenuItem);
+        fileMenu.add(resizeMenuItem);
         mainMenuBar.setBackground(Color.MAGENTA);
         fileMenu.addSeparator();
         fileMenu.add(exitMenuItem);
         newMenuItem.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                throw new UnsupportedOperationException("Not supported yet.");
+                newMenuItemActionPerformed(e);
             }
         });
 
@@ -79,8 +97,44 @@ public class MouseDrawing extends JFrame {
                 exitMenuItemActionPerformed(e);
             }
         });
+        //color action
+        blueMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                blueMenuItemActionPerformed(e);
+            }
+        });
 
-        drawPanel.setPreferredSize(new Dimension(500, 400));
+        redMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                redMenuItemActionPerformed(e);
+            }
+        });
+
+        magentaMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                magentaMenuItemActionPerformed(e);
+            }
+        });
+
+        greenMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                greenMenuItemActionPerformed(e);
+            }
+        });
+        //end of color action
+
+        /*  resizeMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                resizeMenuItemMenuItemActionPerformed(e);
+            }
+
+        }); */
+        drawPanel.setPreferredSize(new Dimension(Panelx * 2, Panely * 2));
         drawPanel.setBackground(Color.BLACK);
         GridBagConstraints gridConstraints = new GridBagConstraints();
         gridConstraints.gridx = 0;
@@ -88,13 +142,12 @@ public class MouseDrawing extends JFrame {
         gridConstraints.gridheight = 2;
         gridConstraints.insets = new Insets(10, 10, 10, 10);
         getContentPane().add(drawPanel, gridConstraints);
-       
+
         drawPanel.addMouseListener(new MouseAdapter() {
             public void mousePressed(MouseEvent e) {
                 drawPanelMousePressed(e);
             }
 
-           
         });
         drawPanel.addMouseMotionListener(new MouseAdapter() {
             public void mouseDragged(MouseEvent e) {
@@ -103,10 +156,9 @@ public class MouseDrawing extends JFrame {
         });
         drawPanel.addMouseMotionListener(new MouseAdapter() {
             public void mouseReleased(MouseEvent e) {
-               drawPanelmouseReleased(e);
+                drawPanelmouseReleased(e);
             }
         });
-               
 
         leftColorLabel.setPreferredSize(new Dimension(40, 40));
         leftColorLabel.setOpaque(true);
@@ -214,17 +266,84 @@ public class MouseDrawing extends JFrame {
             exitForm(null);
         }
     }
-    
-     private void drawPanelMousePressed(MouseEvent e) {
-                throw new UnsupportedOperationException("Not supported yet.");
-            }
-     
-      private void drawPanelmouseDragged(MouseEvent e) {
-                throw new UnsupportedOperationException("Not supported yet.");
-            }
-      
-       private void drawPanelmouseReleased(MouseEvent e) {
-                throw new UnsupportedOperationException("Not supported yet."); 
-            }
 
+    private void drawPanelMousePressed(MouseEvent e) {
+        if (e.getButton() == MouseEvent.BUTTON1 || e.getButton() == MouseEvent.BUTTON3) {
+
+            xPrevious = e.getX();
+            yPrevious = e.getY();
+            if (e.getButton() == MouseEvent.BUTTON1) {
+                drawColor = leftColor;
+            } else { //includes both middle and right
+                drawColor = rightColor;
+            }
+        }
+    }
+
+    private void drawPanelmouseDragged(MouseEvent e) {
+        Line2D.Double myLine = new Line2D.Double(xPrevious, yPrevious, e.getX(), e.getY());
+        g2D.setPaint(drawColor);
+        g2D.draw(myLine);
+        xPrevious = e.getX();
+        yPrevious = e.getY();
+    }
+
+    private void drawPanelmouseReleased(MouseEvent e) {
+        if (e.getButton() == MouseEvent.BUTTON1 || e.getButton() == MouseEvent.BUTTON3) {
+            Line2D.Double myLine = new Line2D.Double(xPrevious, yPrevious, e.getX(), e.getY());
+            g2D.setPaint(drawColor);
+            g2D.draw(myLine);
+        }
+    }
+
+    private void newMenuItemActionPerformed(ActionEvent e) {
+
+        int response;
+        response = JOptionPane.showConfirmDialog(null, "Are you sure you want to start a new drawing?", "New Drawing",
+                JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+        if (response == JOptionPane.YES_OPTION) {
+            g2D.setPaint(drawPanel.getBackground());
+            g2D.fill(new Rectangle2D.Double(0, 0, drawPanel.getWidth(), drawPanel.getHeight()));
+        }
+    }
+
+    /*
+    private void resizeMenuItemMenuItemActionPerformed(ActionEvent e) {
+        String responseX;
+        responseX = JOptionPane.showInputDialog(null, "What is your X dimension?");
+        Panelx = responseX;
+        String responseY;
+        responseY = JOptionPane.showConfirmDialog(null, "What is your Y dimension?");
+        Panely = responseY;
+    }
+     */
+
+    private void blueMenuItemActionPerformed(ActionEvent e) {
+
+        g2D.setPaint(Color.BLUE);
+        g2D.fill(new Rectangle2D.Double(0, 0, drawPanel.getWidth(), drawPanel.getHeight()));
+
+    }
+
+    private void redMenuItemActionPerformed(ActionEvent e) {
+
+        g2D.setPaint(Color.RED);
+        g2D.fill(new Rectangle2D.Double(0, 0, drawPanel.getWidth(), drawPanel.getHeight()));
+
+    }
+
+    private void magentaMenuItemActionPerformed(ActionEvent e) {
+
+        g2D.setPaint(Color.MAGENTA);
+        g2D.fill(new Rectangle2D.Double(0, 0, drawPanel.getWidth(), drawPanel.getHeight()));
+
+    }
+
+    private void greenMenuItemActionPerformed(ActionEvent e) {
+
+        g2D.setPaint(Color.GREEN);
+        
+        g2D.fill(new Rectangle2D.Double(0, 0, drawPanel.getWidth(), drawPanel.getHeight()));
+
+    }
 }
