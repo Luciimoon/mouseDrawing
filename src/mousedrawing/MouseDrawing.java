66ceab99d638a1729c352085c80cb8f9f0309459
compsci <compsci@compsci>
2018-03-16 10:45:14 -0600

    pen

--- src/mousedrawing/MouseDrawing.java
+++ src/mousedrawing/MouseDrawing.java
@@ -24,6 +24,7 @@ public class MouseDrawing extends JFrame {
     JMenuBar mainMenuBar = new JMenuBar();
     JMenu fileMenu = new JMenu("file");
     JMenu canvasMenu = new JMenu("Canvas");
+    JMenu penMenu = new JMenu("Pen");
     JMenuItem newMenuItem = new JMenuItem("Clear");
     JMenuItem exitMenuItem = new JMenuItem("Exit");
     JMenuItem resizeMenuItem = new JMenuItem("Resize");
@@ -32,6 +33,10 @@ public class MouseDrawing extends JFrame {
     JMenuItem redMenuItem = new JMenuItem("Red");
     JMenuItem magentaMenuItem = new JMenuItem("Magenta");
     JMenuItem greenMenuItem = new JMenuItem("Green");
+
+    JMenuItem smallMenuItem = new JMenuItem("Small");
+    JMenuItem mediumMenuItem = new JMenuItem("Medium");
+    JMenuItem largeMenuItem = new JMenuItem("Large");
 
     JPanel drawPanel = new JPanel();
     JLabel leftColorLabel = new JLabel();
@@ -43,6 +48,7 @@ public class MouseDrawing extends JFrame {
     Color drawColor, leftColor, rightColor;
     int Panelx = 500;
     int Panely = 400;
+    int penSize = 1;
 
     /**
      *
@@ -75,10 +81,14 @@ public class MouseDrawing extends JFrame {
         fileMenu.setMnemonic('F');
         mainMenuBar.add(fileMenu);
         mainMenuBar.add(canvasMenu);
+        mainMenuBar.add(penMenu);
         canvasMenu.add(blueMenuItem);
         canvasMenu.add(redMenuItem);
         canvasMenu.add(magentaMenuItem);
         canvasMenu.add(greenMenuItem);
+        penMenu.add(smallMenuItem);
+        penMenu.add(mediumMenuItem);
+        penMenu.add(largeMenuItem);
         fileMenu.add(newMenuItem);
         fileMenu.add(resizeMenuItem);
         mainMenuBar.setBackground(Color.MAGENTA);
@@ -126,6 +136,28 @@ public class MouseDrawing extends JFrame {
             }
         });
         //end of color action
+
+        //pen size
+        smallMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                smallMenuItemActionPerformed(e);
+            }
+        });
+
+        mediumMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                mediumMenuItemActionPerformed(e);
+            }
+        });
+
+        largeMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                largeMenuItemActionPerformed(e);
+            }
+        });
 
         /*  resizeMenuItem.addActionListener(new ActionListener() {
             @Override
@@ -281,18 +313,124 @@ public class MouseDrawing extends JFrame {
     }
 
     private void drawPanelmouseDragged(MouseEvent e) {
-        Line2D.Double myLine = new Line2D.Double(xPrevious, yPrevious, e.getX(), e.getY());
-        g2D.setPaint(drawColor);
-        g2D.draw(myLine);
-        xPrevious = e.getX();
-        yPrevious = e.getY();
+
+        if (penSize == 1) {
+
+            Line2D.Double myLine = new Line2D.Double(xPrevious, yPrevious, e.getX(), e.getY());
+
+            g2D.setPaint(drawColor);
+            g2D.draw(myLine);
+            xPrevious = e.getX();
+            yPrevious = e.getY();
+        } else if (penSize == 2) {
+
+            Line2D.Double myLine0 = new Line2D.Double(xPrevious, yPrevious, e.getX(), e.getY());
+            Line2D.Double myLine1 = new Line2D.Double(xPrevious - 1, yPrevious, e.getX(), e.getY());
+            Line2D.Double myLine2 = new Line2D.Double(xPrevious + 1, yPrevious, e.getX(), e.getY());
+            Line2D.Double myLine3 = new Line2D.Double(xPrevious, yPrevious + 1, e.getX(), e.getY());
+            Line2D.Double myLine4 = new Line2D.Double(xPrevious, yPrevious - 1, e.getX(), e.getY());
+            Line2D.Double myLine5 = new Line2D.Double(xPrevious + 1, yPrevious + 1, e.getX(), e.getY());
+            Line2D.Double myLine6 = new Line2D.Double(xPrevious - 1, yPrevious - 1, e.getX(), e.getY());
+            Line2D.Double myLine7 = new Line2D.Double(xPrevious + 1, yPrevious - 1 , e.getX(), e.getY());
+            Line2D.Double myLine8 = new Line2D.Double(xPrevious - 1, yPrevious + 1, e.getX(), e.getY());
+            Line2D.Double myLine9 = new Line2D.Double(xPrevious - 1, yPrevious, e.getX(), e.getY());
+            Line2D.Double myLine10 = new Line2D.Double(xPrevious - 2, yPrevious, e.getX(), e.getY());
+
+            g2D.setPaint(drawColor);
+            g2D.draw(myLine0);
+            g2D.draw(myLine1);
+            g2D.draw(myLine2);
+            g2D.draw(myLine3);
+            g2D.draw(myLine4);
+            g2D.draw(myLine5);
+            g2D.draw(myLine6);
+            g2D.draw(myLine7);
+            g2D.draw(myLine8);
+            g2D.draw(myLine9);
+            g2D.draw(myLine10);
+            xPrevious = e.getX();
+            yPrevious = e.getY();
+        } else if (penSize == 3) {
+            
+            Line2D.Double myLine0 = new Line2D.Double(xPrevious, yPrevious, e.getX(), e.getY());
+            Line2D.Double myLine1 = new Line2D.Double(xPrevious - 1, yPrevious, e.getX(), e.getY());
+            Line2D.Double myLine2 = new Line2D.Double(xPrevious + 1, yPrevious, e.getX(), e.getY());
+            Line2D.Double myLine3 = new Line2D.Double(xPrevious, yPrevious + 1, e.getX(), e.getY());
+            Line2D.Double myLine4 = new Line2D.Double(xPrevious, yPrevious - 1, e.getX(), e.getY());
+            Line2D.Double myLine5 = new Line2D.Double(xPrevious + 1, yPrevious + 1, e.getX(), e.getY());
+            Line2D.Double myLine6 = new Line2D.Double(xPrevious - 1, yPrevious - 1, e.getX(), e.getY());
+            Line2D.Double myLine7 = new Line2D.Double(xPrevious + 1, yPrevious - 1 , e.getX(), e.getY());
+            Line2D.Double myLine8 = new Line2D.Double(xPrevious - 1, yPrevious + 1, e.getX(), e.getY());
+            Line2D.Double myLine9 = new Line2D.Double(xPrevious - 1, yPrevious, e.getX(), e.getY());
+
+            Line2D.Double myLine11 = new Line2D.Double(xPrevious - 2, yPrevious, e.getX(), e.getY());
+            Line2D.Double myLine12 = new Line2D.Double(xPrevious + 2, yPrevious, e.getX(), e.getY());
+            Line2D.Double myLine13 = new Line2D.Double(xPrevious, yPrevious + 2, e.getX(), e.getY());
+            Line2D.Double myLine14 = new Line2D.Double(xPrevious, yPrevious - 2, e.getX(), e.getY());
+            Line2D.Double myLine15 = new Line2D.Double(xPrevious + 2, yPrevious + 2, e.getX(), e.getY());
+            Line2D.Double myLine16 = new Line2D.Double(xPrevious - 2, yPrevious - 2, e.getX(), e.getY());
+            Line2D.Double myLine17 = new Line2D.Double(xPrevious + 2, yPrevious - 2 , e.getX(), e.getY());
+            Line2D.Double myLine18 = new Line2D.Double(xPrevious - 2, yPrevious + 2, e.getX(), e.getY());
+            Line2D.Double myLine19 = new Line2D.Double(xPrevious - 2, yPrevious, e.getX(), e.getY());
+            
+            Line2D.Double myLine20 = new Line2D.Double(xPrevious - 3, yPrevious, e.getX(), e.getY());
+            Line2D.Double myLine21 = new Line2D.Double(xPrevious + 3, yPrevious, e.getX(), e.getY());
+            Line2D.Double myLine22 = new Line2D.Double(xPrevious, yPrevious + 3, e.getX(), e.getY());
+            Line2D.Double myLine23 = new Line2D.Double(xPrevious, yPrevious - 3, e.getX(), e.getY());
+            Line2D.Double myLine24 = new Line2D.Double(xPrevious + 3, yPrevious + 3, e.getX(), e.getY());
+            Line2D.Double myLine25 = new Line2D.Double(xPrevious - 3, yPrevious - 3, e.getX(), e.getY());
+            Line2D.Double myLine26 = new Line2D.Double(xPrevious + 3, yPrevious - 3 , e.getX(), e.getY());
+            Line2D.Double myLine27 = new Line2D.Double(xPrevious - 3, yPrevious + 3, e.getX(), e.getY());
+            Line2D.Double myLine28 = new Line2D.Double(xPrevious - 3, yPrevious, e.getX(), e.getY());
+
+            g2D.setPaint(drawColor);
+            g2D.draw(myLine0);
+            g2D.draw(myLine1);
+            g2D.draw(myLine2);
+            g2D.draw(myLine3);
+            g2D.draw(myLine4);
+            g2D.draw(myLine5);
+            g2D.draw(myLine6);
+            g2D.draw(myLine7);
+            g2D.draw(myLine8);
+            g2D.draw(myLine9);
+            g2D.draw(myLine11);
+
+            g2D.draw(myLine1);
+            g2D.draw(myLine12);
+            g2D.draw(myLine13);
+            g2D.draw(myLine14);
+            g2D.draw(myLine15);
+            g2D.draw(myLine16);
+            g2D.draw(myLine17);
+            g2D.draw(myLine18);
+            g2D.draw(myLine19);
+            
+            g2D.draw(myLine20);
+            g2D.draw(myLine21);
+            g2D.draw(myLine22);
+            g2D.draw(myLine23);
+            g2D.draw(myLine24);
+            g2D.draw(myLine25);
+            g2D.draw(myLine26);
+            g2D.draw(myLine27);
+            g2D.draw(myLine28);
+            xPrevious = e.getX();
+            yPrevious = e.getY();
+            
+            
+        }
+        
     }
 
     private void drawPanelmouseReleased(MouseEvent e) {
         if (e.getButton() == MouseEvent.BUTTON1 || e.getButton() == MouseEvent.BUTTON3) {
+
             Line2D.Double myLine = new Line2D.Double(xPrevious, yPrevious, e.getX(), e.getY());
+
             g2D.setPaint(drawColor);
             g2D.draw(myLine);
+
         }
     }
 
@@ -317,7 +455,6 @@ public class MouseDrawing extends JFrame {
         Panely = responseY;
     }
      */
-
     private void blueMenuItemActionPerformed(ActionEvent e) {
 
         g2D.setPaint(Color.BLUE);
@@ -342,8 +479,21 @@ public class MouseDrawing extends JFrame {
     private void greenMenuItemActionPerformed(ActionEvent e) {
 
         g2D.setPaint(Color.GREEN);
-        
+
         g2D.fill(new Rectangle2D.Double(0, 0, drawPanel.getWidth(), drawPanel.getHeight()));
 
+    }
+
+    // pen size
+    private void smallMenuItemActionPerformed(ActionEvent e) {
+        penSize = 1;
+    }
+
+    private void mediumMenuItemActionPerformed(ActionEvent e) {
+        penSize = 2;
+    }
+
+    private void largeMenuItemActionPerformed(ActionEvent e) {
+        penSize = 3;
     }
 }
